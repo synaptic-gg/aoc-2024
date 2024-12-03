5a9633556f7a8dd7a2ce745e3f40cd264c76a28d
synapticsurge <synapticsurge@proton.me>
2024-12-03 21:58:31 +0530

    day3 refactor

--- day3/p2/src/main.rs
+++ day3/p2/src/main.rs
@@ -3,26 +3,25 @@ use regex::Regex;
 
 fn main() {
     let data = input::data().unwrap();
-    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    let do_ = Regex::new(r"do\(\)").unwrap();
-    let dont = Regex::new(r"don't\(\)").unwrap();
+    let re = Regex::new(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)").unwrap();
+    //let do_ = Regex::new(r"do\(\)").unwrap();
+    //let dont = Regex::new(r"don't\(\)").unwrap();
 
     let mut result = 0;
     let mut enable = true;
-    for i in data.split_inclusive(')') {
-        if do_.is_match(i) {
+    for i in re.captures_iter(&data) {
+        let k = i[0].parse::<String>().unwrap();
+        if k == "do()" {
             enable = true;
             continue;
-        }else if dont.is_match(i) {
+        } else if k == "don't()" {
             enable = false;
             continue;
         }
-        if let Some(k) = re.captures(i) {
-            if enable {
-                let a: i32 = k[1].parse().unwrap();
-                let b: i32 = k[2].parse().unwrap();
-                result += a * b;
-            }
+        if enable {
+            let a: i32 = i[1].parse().unwrap();
+            let b: i32 = i[2].parse().unwrap();
+            result += a * b;
         }
     }
 

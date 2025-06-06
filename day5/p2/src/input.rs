95a457f6ce2ee9e5fffb0c5c1630b9606bd89112
synapticsurge <synapticsurge@proton.me>
2025-06-06 21:31:41 +0530

    fix

--- day5/p2/src/input.rs
+++ day5/p2/src/input.rs
@@ -2,16 +2,16 @@ use std::fs::File;
 use std::io::prelude::*;
 
 pub fn data() -> std::io::Result<String> {
-    //let mut file = File::open("input.txt")?;
-    //let mut content = String::new();
-    //file.read_to_string(&mut content)?;
-    let content = "47|53\n97|13\n97|61\n97|47\n75|29\n61|13\n75|53\n29|13\n97|29\n53|29\n61|53\n97|53\n61|29\n47|13\n75|47\n97|75\n47|61\n75|61\n47|29\n75|13\n53|13\n\n
-75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47".to_string();
+    let mut file = File::open("input.txt")?;
+    let mut content = String::new();
+    file.read_to_string(&mut content)?;
+//     let content = "47|53\n97|13\n97|61\n97|47\n75|29\n61|13\n75|53\n29|13\n97|29\n53|29\n61|53\n97|53\n61|29\n47|13\n75|47\n97|75\n47|61\n75|61\n47|29\n75|13\n53|13\n\n
+// 75,47,61,53,29
+// 97,61,53,29,13
+// 75,29,13
+// 75,97,47,61,53
+// 61,13,29
+// 97,13,75,29,47".to_string();
 
     Ok(content)
 }
--- day5/p2/src/main.rs
+++ day5/p2/src/main.rs
@@ -1,7 +1,8 @@
-mod input;
-use std::{collections::HashMap};
 
-fn main() {
+mod input;
+use std::collections::{HashMap, HashSet};
+
+pub fn main() {
     let in_data = input::data().unwrap();
     let mut data = in_data.split("\n\n");
     let con = data.next().unwrap();
@@ -9,17 +10,14 @@ fn main() {
     let hash_con = con_hash(con);
     let pages = check_data.split("\n").into_iter();
     let mut result = 0;
-    println!("{:?}", hash_con);
     for page in pages {
-        let checked = check(page, hash_con.clone());
-        if checked {
-            let data = page
-                .split(",")
-                .filter_map(|n| n.trim().parse::<i32>().ok())
-                .collect::<Vec<i32>>();
+        let checked = check(page, &hash_con);
+        if !checked {
+        
+            let data = sort(page,&hash_con);
 
             let y = data.len() / 2;
-            println!("{:?}", data);
+            //println!("{:?}", data);
             if data.len() != 0 {
                 result += data[y]
             }
@@ -27,30 +25,46 @@ fn main() {
     }
 
     println!("{}", result)
-    
 }
 
-fn sort(condition: HashMap<i32, Vec<i32>>,page:Vec<i32>)-> Vec<i32>{
-    let mut out: Vec<i32> = vec![];
-    let mut marked: Vec<i32> = vec![];
-   let condition = condition;
-   let page = page;
-fn dfs_post(condition: HashMap<i32, Vec<i32>>,pag:i32){
-    for w pag{
-        if marked.contains(w){
-            continue;
-        }else {
-            dfs_post(condition, w);
-            marked.push(w)
-        }}}
-dfs_post(condition, page[1]);    
-    
-println!("{:?}",out);
-out
 
+
+pub fn build_graph(rules: &[(i32, i32)], update: &[i32]) -> HashMap<i32, Vec<i32>> {
+    let page_set: HashSet<i32> = update.iter().cloned().collect();
+    let mut graph: HashMap<i32, Vec<i32>> = HashMap::new();
+
+    for &page in update {
+        graph.entry(page).or_default();
+    }
+
+    for &(a, b) in rules {
+        if page_set.contains(&a) && page_set.contains(&b) {
+            graph.get_mut(&a).unwrap().push(b);
+        }
+    }
+
+    graph
 }
 
-fn con_hash(con_data: &str) -> HashMap<i32, Vec<i32>> {
+pub fn dfs(node:i32,graph: &HashMap<i32,Vec<i32>> , visted: &mut HashSet<i32>,temp : &mut HashSet<i32>,sorted : &mut  Vec<i32> )-> bool {
+    if temp.contains(&node){
+        return  false;
+    }
+    if !visted.contains(&node){
+        temp.insert(node);
+        for n in &graph[&node]{
+            if !dfs(*n, graph, visted, temp, sorted){
+                return  false;
+            }
+        }
+        temp.remove(&node);
+        visted.insert(node); 
+        sorted.push(node);
+    }
+    true
+}
+
+pub fn con_hash(con_data: &str) -> HashMap<i32, Vec<i32>> {
     let mut out: HashMap<i32, Vec<i32>> = HashMap::new();
     let con = con_data.split("\n").into_iter();
     for i in con {
@@ -58,22 +72,44 @@ fn con_hash(con_data: &str) -> HashMap<i32, Vec<i32>> {
             .split("|")
             .filter_map(|n| Some(n.trim().parse::<i32>().unwrap()));
         let i2 = val.next().unwrap();
-        //println!("{}",i1);
         let i1 = val.next().unwrap();
-        //println!("{}",i2);
-        if out.contains_key(&i1) {
-            let mut list = out.get(&i1).unwrap().clone();
-            list.push(i2);
-            out.insert(i1, list.to_vec());
-        } else {
-            out.insert(i1, [i2].to_vec());
-        }
+       out.entry(i1).or_default().push(i2);
     }
-
+//println!("{:?}",out);
     out
 }
 
-fn check(check_data: &str, condition: HashMap<i32, Vec<i32>>) -> bool {
+pub fn sort(page: &str, condition: &HashMap<i32, Vec<i32>>) -> Vec<i32> {
+    let data = page
+        .split(",")
+        .filter_map(|n| n.trim().parse::<i32>().ok())
+        .collect::<Vec<i32>>();
+    let mut rules = vec![];
+    for (k, v) in condition.iter() {
+        for to in v {
+            rules.push((*k, *to));
+        }
+    }
+
+    let graph = build_graph(&rules, &data);
+    let mut visited = HashSet::new();
+    let mut temp = HashSet::new();
+    let mut sorted = vec![];
+
+    for &node in &data {
+        if !visited.contains(&node) {
+           let _ =  !dfs(node, &graph, &mut visited, &mut temp, &mut sorted) ;
+                
+            
+        }
+    }
+
+    sorted.reverse();
+    sorted
+}
+
+
+pub fn check(check_data: &str, condition: &HashMap<i32, Vec<i32>>) -> bool {
     let data = check_data
         .split(",")
         .filter_map(|n| n.trim().parse::<i32>().ok())
